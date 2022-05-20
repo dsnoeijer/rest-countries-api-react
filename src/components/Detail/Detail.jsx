cc0ce1ea44e3b9f302a5931359700d74d6bf3c15
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-05-21 01:35:09 +0200

    fixed results with not-existing values

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -8,19 +8,22 @@ const Detail = () => {
     // eslint-disable-next-line
     const [state, dispatch] = useContext(Context);
     const country = state.country;
-    console.log(country)
 
     const getCurrencies = () => {
         let curs = '';
-        let i = 0;
-        Object.keys(country.currencies).map((cur) => {
-            if (i < Object.keys(country.currencies).length - 1) {
-                i++;
-                return curs += `${country.currencies[cur].name}, `;
-            } else {
-                return curs += `${country.currencies[cur].name}`;
-            }
-        })
+        if (country.currencies) {
+            let i = 0;
+            Object.keys(country.currencies).map((cur) => {
+                if (i < Object.keys(country.currencies).length - 1) {
+                    i++;
+                    return curs += `${country.currencies[cur].name}, `;
+                } else {
+                    return curs += `${country.currencies[cur].name}`;
+                }
+            })
+        } else {
+            curs += 'N/A';
+        }
         return curs;
     }
 
@@ -28,14 +31,18 @@ const Detail = () => {
         let lang = '';
         let i = 0;
 
-        Object.values(country.languages).map((language) => {
-            if (i < Object.values(country.languages).length - 1) {
-                i++;
-                return lang += `${language}, `;
-            } else {
-                return lang += `${language}`;
-            }
-        })
+        if (country.languages) {
+            Object.values(country.languages).map((language) => {
+                if (i < Object.values(country.languages).length - 1) {
+                    i++;
+                    return lang += `${language}, `;
+                } else {
+                    return lang += `${language}`;
+                }
+            });
+        } else {
+            lang += 'N/A';
+        }
         return lang;
     }
 
@@ -51,24 +58,24 @@ const Detail = () => {
                     <div className='detail-top-div'>
                         <h1>{country.name.common}</h1>
                         <p>
-                            Native Name: <span className="detail-result"> {Object.values(country.name.nativeName)[0].official} </span>
+                            Native Name: <span className="detail-result"> {country.name.nativeName ? Object.values(country.name.nativeName)[0].official : 'N/A'} </span>
                         </p>
                         <p>
-                            Population: <span className="detail-result">{country.population.toLocaleString("en-US")}</span>
+                            Population: <span className="detail-result">{country.population ? country.population.toLocaleString("en-US") : 'N/A'}</span>
                         </p>
                         <p>
-                            Region: <span className="detail-result">{country.region}</span>
+                            Region: <span className="detail-result">{country.region ? country.region : 'N/A'}</span>
                         </p>
                         <p>
-                            Sub Region: <span className="detail-result">{country.sybregion}</span>
+                            Sub Region: <span className="detail-result">{country.subregion ? country.subregion : 'N/A'}</span>
                         </p>
                         <p>
-                            Capital: <span className="detail-result">{country.capital}</span>
+                            Capital: <span className="detail-result">{country.capital ? country.capital : 'N/A'}</span>
                         </p>
                     </div>
                     <div className='detail-middle-div'>
                         <p>
-                            Top Level Domain: <span className="detail-result">{country.tld[0]}</span>
+                            Top Level Domain: <span className="detail-result">{country.tld ? country.tld[0] : 'N/A'}</span>
                         </p>
                         <p>
                             Currencies: <span className="detail-result">{getCurrencies()}</span>
@@ -80,9 +87,12 @@ const Detail = () => {
                     <div className='detail-bottom-div'>
                         <h2>Border Countries:</h2>
                         <div className="borders">
-                            {Object.values(country.borders).map((border) => {
+                            {country.borders ? Object.values(country.borders).map((border) => {
                                 return <CountryButton key={border} country={border} />
-                            })}
+                            })
+                                :
+                                'N/A'
+                            }
                         </div>
                     </div>
                 </div>
